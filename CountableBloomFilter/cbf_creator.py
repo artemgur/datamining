6ea5254c91d60fbcb6e890358e457deb94b748e1
artemgur <artemgur01@gmail.com>
2021-05-10 14:32:18 +0300

    Task 4 code cleanup

--- CountableBloomFilter/cbf_creator.py
+++ CountableBloomFilter/cbf_creator.py
@@ -1,11 +1,12 @@
 import hasher
 import countable_bloom_filter
-import math
 
 
 def create(length: int, hash_functions_count: int) -> countable_bloom_filter.CountableBloomFilter:
     return countable_bloom_filter.CountableBloomFilter(length, *list(map(lambda x: hasher.Hasher().hash, [None] * hash_functions_count)))
 
 def create_optimal(false_positive_probability: float, expected_elements_count: int) -> countable_bloom_filter.CountableBloomFilter:
-    length = math.ceil(expected_elements_count * math.log(false_positive_probability) / (math.log(1 / 2 ** math.log(2))))
-    return create(length, countable_bloom_filter.optimal_hash_functions_count(length, false_positive_probability, expected_elements_count))
+    length = countable_bloom_filter.get_optimal_length(expected_elements_count, false_positive_probability)
+    return create(length, countable_bloom_filter.get_optimal_hash_functions_count(length, expected_elements_count))
+
+
--- CountableBloomFilter/countable_bloom_filter.py
+++ CountableBloomFilter/countable_bloom_filter.py
@@ -3,21 +3,6 @@ import math
 
 T = TypeVar('T')
 
-# def _sum_lists(a: list[int], b: list[int]) -> list[int]:
-#     return Enumerable(a).zip(Enumerable(b), lambda x, y: x + y).to_list()
-
-# def _or_lists(a: list[int], b: list[int]) -> list[int]:
-#     return Enumerable(a).zip(Enumerable(b), lambda x, y: _or(x, y)).to_list()
-
-# def _subtract_list(a: list[int], b: list[int]) -> list[int]:
-#     return Enumerable(a).zip(Enumerable(b), lambda x, y: x - y).to_list()
-
-# def _or(a: int, b: int):
-#     return a != 0 or b != 0
-
-# def _implication(a: int, b: int):
-#     return a == 0 or b != 0
-
 class CountableBloomFilter(Generic[T]):
     def __init__(self, length: int, *args: Callable[[T], int]):
         if len(args) == 0:
@@ -62,29 +47,8 @@ class CountableBloomFilter(Generic[T]):
     def get_false_positive_probability(self):
         return (1 - math.exp(-len(self.__hash_functions) * self.__elements_count / len(self.__bloom_filter))) ** len(self.__hash_functions)
 
-def optimal_hash_functions_count(length: int, false_positive_probability: float, expected_elements_count: int) -> int:
+def get_optimal_hash_functions_count(length: int, expected_elements_count: int) -> int:
     return round(math.log(2) * length / expected_elements_count)
 
-# def _string_to_char_list(string: str, max_length: int):
-#     string_list = []
-#     string_list[0:max_length] = string # Supposed to convert string to the list of characters https://www.geeksforgeeks.org/python-splitting-string-to-list-of-characters/
-#     return string_list
-
-# __hex_chars_dictionary = {'0': [0, 0, 0, 0], '1': [0, 0, 0, 1], '2': [0, 0, 1, 0], '3': [0, 0, 1, 1],
-#                           '4': [0, 1, 0, 0], '5': [0, 1, 0, 1], '6': [0, 1, 1, 0], '7': [0, 1, 1, 1],
-#                           '8': [1, 0, 0, 0], '9': [1, 0, 0, 1], 'A': [1, 0, 1, 0], 'B': [1, 0, 1, 1],
-#                           'C': [1, 1, 0, 0], 'D': [1, 1, 0, 1], 'E': [1, 1, 1, 0], 'F': [1, 1, 1, 1]}
-#
-#
-# def __hex_char_to_binary_list(hex_char: str) -> list[int]:
-#     if len(hex_char) != 1:
-#         raise ValueError()
-#     return __hex_chars_dictionary[hex_char]
-#
-#
-# def __hex_to_binary_list(hex_string: str, length: int) -> list[int]:
-#     string_list = []
-#     string_list[0:length] = hex_string # Supposed to convert string to the list of characters https://www.geeksforgeeks.org/python-splitting-string-to-list-of-characters/
-#     return Enumerable(string_list).select(lambda x: __hex_char_to_binary_list(x)).aggregate(lambda x, y: x + y, [])
-
-
+def get_optimal_length(expected_elements_count, false_positive_probability):
+    return math.ceil(expected_elements_count * math.log(false_positive_probability) / (math.log(1 / 2 ** math.log(2))))
